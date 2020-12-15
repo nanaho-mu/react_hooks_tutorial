b0be73ff180335dd4f36065f4fa41fdf380eb5ec
Nanaho <nanaho@NanahonoMacBook.local>
2020-12-15 17:31:42 +0900

    implement component using multiple sates

--- src/App.js
+++ src/App.js
@@ -1,53 +1,29 @@
 import React,{useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App=()=>{
-  const [count,setCount]=useState(0)
-  const increment=()=>{
-    setCount(count+1)
-  }
-  const decrement=()=>{
-    setCount(count-1)
-  }
-  const increment2=()=>{
-    setCount(prev=>prev+1)
-  }
+const App=(props)=>{
+  const [name,setName]=useState(props.name)
+  const [price,setPrice]=useState(props.price)
   const reset=()=>{
-    setCount(0)
+    setPrice(props.price)
+    setName(props.name)
   }
-  const modify=()=>{
-    setCount(prev=>prev*2)//1行で書いて｛｝を省略したらreturnが省略できる
-  }
-  const divide3=()=>{
-    if(count%3===0){
-      setCount(count/3)
-    }
-  }
-  const divide3_2=()=>setCount(prev=>{
-    return prev%3===0?prev/3:prev
-  })
-  
-
   return (
-    <div>
-      <div>count:{count}</div>
-      <div>
-       <button onClick={increment}>+1</button>
-       <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-       <button onClick={increment2}>+1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={modify}>×2</button>
-        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
-        <button onClick={divide3_2}>3の倍数の時だけ3で割る</button>
-      </div>
-    </div>
+   <div>
 
+     <p>現在の{name}は、{price}円です</p>
+     <button onClick={()=>setPrice(price+1)}>+1</button>
+     <button onClick={()=>setPrice(price-1)}>-1</button>
+     <button onClick={reset}>reset</button>
+     <input value={name} onChange={(e)=>setName(e.target.value)}/>
+    </div>
+    
   );
+}
+
+App.defaultProps={
+  name:'',
+  price:1000
 }
 
 export default App;
